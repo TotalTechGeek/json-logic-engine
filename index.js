3b020e7834729c0859ca0df8026e07a7a06faa07
Julian Laubstein <contact@julianlaubstein.de>
2021-10-25 09:01:44 +0200

    Made index backwards compatible

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // @ts-check
-'use strict'
+"use strict";
 
-export { default as LogicEngine } from './logic'
-export { default as AsyncLogicEngine } from './asyncLogic'
+import LogicEngine from "./logic";
+import AsyncLogicEngine from "./asyncLogic";
+
+export { LogicEngine };
+export { AsyncLogicEngine };
+
+export default { LogicEngine, AsyncLogicEngine };
