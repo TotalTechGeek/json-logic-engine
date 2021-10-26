5254b714a3506f9c2797d911f41ef45b61dfa4c0
TotalTechGeek <TotalTechGeek@users.noreply.github.com>
2021-10-25 19:58:40 -0500

    Add some new items to the exports

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
 
 import LogicEngine from './logic.js'
 import AsyncLogicEngine from './asyncLogic.js'
+import Compiler from './compiler.js'
+import Yield from './structures/Yield.js'
+import EngineObject from './structures/EngineObject.js'
+import Constants from './constants.js'
 
 export { LogicEngine }
 export { AsyncLogicEngine }
+export { Compiler }
+export { Yield }
+export { EngineObject }
+export { Constants }
 
-export default { LogicEngine, AsyncLogicEngine }
+export default { LogicEngine, AsyncLogicEngine, Compiler, Yield, EngineObject, Constants }
