025ecba9d3e34121da84c6dcd06fc88a8405c589
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2023-05-05 02:19:39 -0400

    Additional bun modifications (ensuring decent perf)

--- utilities/declareSync.js
+++ utilities/declareSync.js
@@ -2,7 +2,7 @@
 'use strict'
 
 import { Sync } from '../constants.js'
-export default (function declareSync (obj, sync = true) {
+export default function declareSync (obj, sync = true) {
   obj[Sync] = sync
   return obj
-})
+}
