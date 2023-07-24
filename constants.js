2968cd9c9715a03cbb78d0eec3c4bc41d68081a8
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2023-07-24 13:44:58 -0500

    Use Symbol.for in case there are multiple instances of the package

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 // @ts-check
 'use strict'
 
-export const Sync = Symbol('_sync')
-export const Override = Symbol('_override')
-export const EfficientTop = Symbol('_efficientTop')
+export const Sync = Symbol.for('json_logic_sync')
+export const Override = Symbol.for('json_logic_override')
+export const EfficientTop = Symbol.for('json_logic_efficientTop')
 export const isSync = (x) => Boolean(typeof x !== 'function' || x[Sync])
 export default {
   Sync,
