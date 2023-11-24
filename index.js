408d447deea4dd1f128828baad27b29892003e49
Jesse Mitchell <jesse.daniel.mitchell@gmail.com>
2023-11-24 09:27:37 -0600

    Add support for dot-prop syntax to resolve #23

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ import EngineObject from './structures/EngineObject.js'
 import Constants from './constants.js'
 import defaultMethods from './defaultMethods.js'
 import { asLogicSync, asLogicAsync } from './asLogic.js'
+import { splitPath } from './utilities/splitPath.js'
 
+export { splitPath }
 export { LogicEngine }
 export { AsyncLogicEngine }
 export { Compiler }
@@ -20,4 +22,4 @@ export { defaultMethods }
 export { asLogicSync }
 export { asLogicAsync }
 
-export default { LogicEngine, AsyncLogicEngine, Compiler, YieldStructure, EngineObject, Constants, defaultMethods, asLogicSync, asLogicAsync }
+export default { LogicEngine, AsyncLogicEngine, Compiler, YieldStructure, EngineObject, Constants, defaultMethods, asLogicSync, asLogicAsync, splitPath }
